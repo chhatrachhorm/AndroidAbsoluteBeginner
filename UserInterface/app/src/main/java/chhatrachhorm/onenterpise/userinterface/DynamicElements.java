8ca99aade6eed4ad03350d75ef1d3442955b2cdf
Chhorm Chhatra <ch.chhatra@gmail.com>
2017-09-13 09:21:22 +0700

    Dynamic Elements

--- UserInterface/app/src/main/java/chhatrachhorm/onenterpise/userinterface/DynamicElements.java
+++ UserInterface/app/src/main/java/chhatrachhorm/onenterpise/userinterface/DynamicElements.java
@@ -1,7 +1,11 @@
 package chhatrachhorm.onenterpise.userinterface;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -11,7 +15,7 @@ import android.widget.Toast;
 
 
 /**
- *
+ * Dynamic Layout
  * Created by chhatra on 9/12/2017.
  */
 
@@ -23,19 +27,22 @@ public class DynamicElements extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic);
+
         mFrame = findViewById(R.id.ad_frame_layout);
+
         // set pages
         for(int i = 0 ; i <100; i++){
+            // make text view
             TextView mTextView = new TextView(this);
             mTextView.setId(i+1);
             mTextView.setText(getString(R.string.page_number, (i+1)));
+            // make linear layout
             LinearLayout mLinearLayout = new LinearLayout(this);
             mLinearLayout.setId(i);
             mLinearLayout.setOrientation(LinearLayout.VERTICAL);
             mLinearLayout.addView(mTextView);
             mLinearLayout.setVisibility(View.INVISIBLE);
             mFrame.addView(mLinearLayout);
-
         }
 
         pages = mFrame.getChildCount();
@@ -43,9 +50,6 @@ public class DynamicElements extends AppCompatActivity {
 
         final Button mPrev = findViewById(R.id.ad_prev_btn);
         final Button mNext = findViewById(R.id.ad_next_btn);
-
-
-
 
         mPrev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,5 +79,28 @@ public class DynamicElements extends AppCompatActivity {
             mFrame.getChildAt(newIndex).setVisibility(View.VISIBLE);
             this.index = newIndex;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.dynamic_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.menu_main:
+                startActivity(new Intent(DynamicElements.this, MainActivity.class));
+                finish();
+                break;
+            case R.id.menu_dev:
+                Intent me = new Intent(Intent.ACTION_VIEW);
+                me.setData(Uri.parse(getString(R.string.my_url)));
+                startActivity(me);
+        }
+        return true;
     }
 }
