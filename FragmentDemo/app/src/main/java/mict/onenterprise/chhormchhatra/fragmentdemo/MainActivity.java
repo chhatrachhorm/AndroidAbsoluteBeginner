361a76ed01fc836988da27e12c69cf4b8a786f4e
Chhorm Chhatra <ch.chhatra@gmail.com>
2017-09-23 09:50:11 +0700

    FragementDemo

--- FragmentDemo/app/src/main/java/mict/onenterprise/chhormchhatra/fragmentdemo/MainActivity.java
+++ FragmentDemo/app/src/main/java/mict/onenterprise/chhormchhatra/fragmentdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package mict.onenterprise.chhormchhatra.fragmentdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mPLA = findViewById(R.id.main_pla_btn);
+        mPLA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PortLandActivity.class));
+            }
+        });
     }
 }
